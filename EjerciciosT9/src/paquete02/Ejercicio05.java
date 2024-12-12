e61fcf74f241b264081c3078bdcd74f09668e0c2
Emilio Cueva <ecueva611@gmail.com>
2024-12-12 12:45:13 -0500

    Hola

--- EjerciciosT9/src/paquete02/Ejercicio05.java
+++ EjerciciosT9/src/paquete02/Ejercicio05.java
@@ -17,8 +17,20 @@ public class Ejercicio05 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double[] ventas = new double [5]; 
+        String[] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes"};
       
-
+        for(int i = 0; i < dias.length; i++){
+            System.out.printf("ingresar el valor de las ventas del dia %s\n",dias[i]);
+            double venta = entrada.nextDouble();
+            ventas[i] = venta;
+                           
+        }
+        for(int i = 0; i < dias.length; i++){
+            System.out.printf("%s $%.2f\n", dias[i], ventas[i]);        
+                
+        }   
     }
 
 }
